76e15994b93556602b9b012cda958a635bafb9e9
Joao Santana <jjoaoppedro@hotmail.com.br>
2024-06-18 22:27:12 -0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,14 +8,23 @@ public class Main {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
+        BufferedReader bufferedReader = null;
 
         try {
 
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream()
-                    .write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String clientRequest = bufferedReader.readLine();
+            String[] requestParts = clientRequest.split(" ");
+            String path = requestParts[1];
+
+            if ("/".equals(path)) {
+                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else {
+                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
